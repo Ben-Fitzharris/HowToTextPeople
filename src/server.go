cfa33326dea306181f0c0ee52db93f779c797869
Ben-Fitzharris <108950981+Ben-Fitzharris@users.noreply.github.com>
2024-10-17 16:11:27 -0700

    Read and Print Requests
    Reads and prints request to terminal

--- src/server.go
+++ src/server.go
@@ -3,7 +3,7 @@ package main
 import "net"
 
 func createListener() (l net.Listener) {
-	l, err := net.Listen("tcp4", "locallly")
+	l, err := net.Listen("tcp4", "127.0.0.1:6111")
 	if err != nil {
 		println("Create Listener Error: ", err)
 	}
@@ -11,4 +11,8 @@ func createListener() (l net.Listener) {
 	l.Accept()
 
 	return l
+}
+
+func parseRequest(b []byte) {
+
 }
--- src/start.go
+++ src/start.go
@@ -1,10 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
+	//messages := make(chan string)
+
 	fmt.Println("start")
 	l := createListener()
-	l.Close()
+	defer l.Close()
+
+	for {
+
+		connection, err := l.Accept()
+		if err != nil {
+			log.Fatal("Listener Error: ", err)
+		}
+
+		go func(conn net.Conn) {
+			incoming := make([]byte, 1000)
+			num, err := conn.Read(incoming)
+			if err != nil {
+				fmt.Println("Read Error: ", err)
+			}
+			fmt.Println("num: ", num)
+			strIncoming := string(incoming)
+			fmt.Println(strIncoming)
+
+			conn.Close()
+		}(connection)
+	}
+
 	fmt.Println("end")
 }
