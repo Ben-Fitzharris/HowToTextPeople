f8a7c3f1eb4a5b0c1c63a4d1c451cc1511bd0bd3
Ben-Fitzharris <108950981+Ben-Fitzharris@users.noreply.github.com>
2024-10-18 12:41:42 -0700

    http package test
    Testing out go package http to serve a string on /hello

--- src/start.go
+++ src/start.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
-	"net"
+	"net/http"
 )
 
 func main() {
@@ -13,26 +14,38 @@ func main() {
 	l := createListener()
 	defer l.Close()
 
-	for {
+	/*
+		for {
 
-		connection, err := l.Accept()
-		if err != nil {
-			log.Fatal("Listener Error: ", err)
-		}
-
-		go func(conn net.Conn) {
-			incoming := make([]byte, 1000)
-			num, err := conn.Read(incoming)
+			connection, err := l.Accept()
 			if err != nil {
-				fmt.Println("Read Error: ", err)
+				log.Fatal("Listener Error: ", err)
 			}
-			fmt.Println("num: ", num)
-			strIncoming := string(incoming)
-			fmt.Println(strIncoming)
 
-			conn.Close()
-		}(connection)
+
+
+			go func(conn net.Conn) {
+				incoming := make([]byte, 1000)
+				num, err := conn.Read(incoming)
+				if err != nil {
+					fmt.Println("Read Error: ", err)
+				}
+				fmt.Println("num: ", num)
+				strIncoming := string(incoming)
+				fmt.Println(strIncoming)
+
+				conn.Close()
+			}(connection)
+		}
+	*/
+	//http handle server
+
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello Internet Person \n")
 	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe("127.0.0.1:6112", nil))
 
 	fmt.Println("end")
 }
