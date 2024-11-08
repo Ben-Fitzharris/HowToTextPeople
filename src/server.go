b6693e52150426ac62765d9d0b1c2ae7469aee4f
Ben-Fitzharris <108950981+Ben-Fitzharris@users.noreply.github.com>
2024-11-07 17:31:06 -0800

    Request Parse
    Parse Request into struct

--- src/server.go
+++ src/server.go
@@ -1,6 +1,12 @@
 package main
 
-import "net"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"net/http"
+	"strings"
+)
 
 func createListener() (l net.Listener) {
 	l, err := net.Listen("tcp4", "127.0.0.1:6111")
@@ -13,6 +19,25 @@ func createListener() (l net.Listener) {
 	return l
 }
 
-/*func parseRequest(b []byte) {
+func parseRequest(conn net.Conn, b []byte) {
 
-}*/
+	num, err := conn.Read(b)
+	if err != nil {
+		fmt.Println("Read Error: ", err)
+	}
+	fmt.Println(num)
+
+	r, err := http.ReadRequest(bufio.NewReader(strings.NewReader(string(b))))
+	if err != nil {
+		fmt.Println("Error http request parse: ", err)
+	}
+
+	fmt.Println(r.Method)
+	fmt.Println(r.RequestURI)
+
+	if r.Method == "GET" {
+		//getResource(r.RequestURI)
+		//respond
+	}
+
+}
--- src/start.go
+++ src/start.go
@@ -2,54 +2,44 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"log"
-	"net/http"
-	"path"
+	"net"
 )
 
 func main() {
 	//messages := make(chan string)
 
 	fmt.Println("start")
-	//l := createListener()
-	//defer l.Close()
+	l := createListener()
+	defer l.Close()
 
-	/*
-		for {
+	for {
 
-			connection, err := l.Accept()
-			if err != nil {
-				log.Fatal("Listener Error: ", err)
-			}
-
-
-
-			go func(conn net.Conn) {
-				incoming := make([]byte, 1000)
-				num, err := conn.Read(incoming)
-				if err != nil {
-					fmt.Println("Read Error: ", err)
-				}
-				fmt.Println("num: ", num)
-				strIncoming := string(incoming)
-				fmt.Println(strIncoming)
-
-				conn.Close()
-			}(connection)
+		connection, err := l.Accept()
+		if err != nil {
+			log.Fatal("Listener Error: ", err)
 		}
-	*/
-	//http handle server
-	fmt.Println("hello")
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello Internet Person \n")
+
+		go func(conn net.Conn) {
+			incoming := make([]byte, 1000)
+			parseRequest(conn, incoming)
+			conn.Close()
+		}(connection)
 	}
 
-	fs := http.FileServer(http.Dir(path.Dir("./src")))
+	//http handle server
+	/*
+			helloHandler := func(w http.ResponseWriter, req *http.Request) {
+				io.WriteString(w, "Hello Internet Person \n")
+			}
 
-	http.HandleFunc("/hello", helloHandler)
-	http.Handle("/", fs)
-	log.Fatal(http.ListenAndServe("127.0.0.1:6112", nil))
+			fs := http.FileServer(http.Dir(path.Dir("./src")))
 
-	fmt.Println("end")
+			http.HandleFunc("/hello", helloHandler)
+			http.Handle("/", fs)
+			log.Fatal(http.ListenAndServe("127.0.0.1:6112", nil))
+
+
+		fmt.Println("end")
+	*/
 }
